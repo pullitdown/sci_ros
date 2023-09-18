0a18684d8329f4dda747c8c9eedb74971932ab5b
dgz <595259789@qq.com>
2023-09-18 19:46:30 +0800

    ok

--- gray_model.py
+++ gray_model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from loss import LossFunction
+from gray_loss import LossFunction
 
 
 
--- sci_node.py
+++ sci_node.py
@@ -1,4 +1,4 @@
-#!/home/sunteng/workspace/conda/env/py39/bin/python
+#!/home/sunteng/workspace/conda/envs/py39/bin/python
 
 import rospy
 from sensor_msgs.msg import Image
@@ -12,6 +12,8 @@ import cv2
 import torch
 from torchvision import transforms
 
+from gray_model import Finetunemodel
+
 class ImageProcessor:
     def __init__(self):
         # 创建 CvBridge 对象
@@ -20,13 +22,13 @@ class ImageProcessor:
         # 加载你的 PyTorch 模型
         self.device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-        self.model = torch.load(self.model)
+        self.model = Finetunemodel(self.model)
         self.model.eval()
         self.model.to(self.device)
-        sub_image_0 = rospy.get_param('sub_image_0','/cam0/image_raw')
-        sub_image_1 = rospy.get_param('sub_image_1','/cam1/image_raw')
-        pub_image_0 = rospy.get_param('pub_image_0','/cam0/image_enhance')
-        pub_image_1 = rospy.get_param('pub_image_1','/cam1/image_enhance')
+        sub_image_0 = rospy.get_param('/sci_node/sub_image_0','/cam0/image_raw')
+        sub_image_1 = rospy.get_param('/sci_node/sub_image_1','/cam1/image_raw')
+        pub_image_0 = rospy.get_param('/sci_node/pub_image_0','/cam0/image_enhance')
+        pub_image_1 = rospy.get_param('/sci_node/pub_image_1','/cam1/image_enhance')
         # 定义图像预处理流程
         self.preprocess = transforms.Compose([
             transforms.ToTensor(),
@@ -45,11 +47,10 @@ class ImageProcessor:
         # 将 ROS 图像消息转换为 OpenCV 图像
         cv_image_0 = self.bridge.imgmsg_to_cv2(msg_0, "mono8")
         cv_image_1 = self.bridge.imgmsg_to_cv2(msg_1, "mono8")
-        
+        input_tensor_0 = self.preprocess(cv_image_0)
+        input_tensor_1 = self.preprocess(cv_image_1)
+        input_batch = torch.stack([input_tensor_0, input_tensor_1]).to(device=self.device)
 
-        # 将 OpenCV 图像转换为 PyTorch 张量
-        input_tensor = self.preprocess([cv_image_0,cv_image_1])
-        input_batch = input_tensor.unsqueeze(0).to(self.device)
 
         # 将图像输入到模型中
         with torch.no_grad():
@@ -63,17 +64,17 @@ class ImageProcessor:
         msg_out_1 = self.bridge.cv2_to_imgmsg(output_image[1], "mono8")
 
         # 发布处理后的图像
-        self.pub.publish(msg_out_0)
-        self.pub.publish(msg_out_1)
+        self.pub_left.publish(msg_out_0)
+        self.pub_right.publish(msg_out_1)
 
     def output_to_image(self, output):
         # 根据你的模型和需求，将模型输出转换为图像
         # 这里是一个简单的例子，将输出张量转换为 8 位的灰度图像
-        output_image_0 = (output.detach().cpu()[1][0].squeeze().numpy() * 255).astype('uint8')
-        output_image_1 = (output.detach().cpu()[1][1].squeeze().numpy() * 255).astype('uint8')
+        output_image_0 = (output[1][0].detach().cpu().squeeze().numpy() * 255).astype('uint8')
+        output_image_1 = (output[1][1].detach().cpu().squeeze().numpy() * 255).astype('uint8')
         return output_image_0,output_image_1
 
 if __name__ == '__main__':
-    rospy.init_node('image_processor')
+    rospy.init_node('sci_node')
     image_processor = ImageProcessor()
     rospy.spin()
